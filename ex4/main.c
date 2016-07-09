a6fa7c68f046d268af6cd09d9932e157d4cba347
Bernardo Amorim <contato@bamorim.com>
2016-07-09 19:10:39 -0300

    Run many times

--- ex4/main.c
+++ ex4/main.c
@@ -18,22 +18,15 @@ int* generate_numbers(size_t size) {
   return numbers;
 }
 
-int main(int argc, char ** argv) {
-  if(argc != 2){
-    printf("Usage: ./quicksort k");
-    exit(1);
-  }
-  int k = atoi(argv[1]);
-  size_t size = (size_t) pow(2, (double) k);
+float bench(size_t size){
   int* numbers = generate_numbers(size);
-  size_t i;
 
   clock_t start = clock();
   ip_quicksort(numbers,0,size-1);
   clock_t end = clock();
   float seconds = (float)(end - start) / CLOCKS_PER_SEC;
 
-  printf("Sorted in %f seconds.\n", seconds);
+  size_t i;
   printf("Checking...\n");
 
   for(i = 1; i < size; i++){
@@ -45,6 +38,25 @@ int main(int argc, char ** argv) {
       printf("Valid ordering\n");
     }
   }
+
   free(numbers);
+  return seconds;
+}
+
+int main(int argc, char ** argv) {
+  if(argc < 3){
+    printf("Usage: ./quicksort k times\n");
+    exit(1);
+  }
+  int k = atoi(argv[1]);
+  int times = atoi(argv[2]);
+  size_t size = (size_t) pow(2, (double) k);
+  float seconds = 0;
+  int i = 0;
+  for(i = 0; i < times; i++){
+    seconds += bench(size);
+  }
+
+  printf("Sorted %d times in %f seconds.\nMean: %f", times, seconds, seconds/times);
   return 0;
 }
