c358b0fa3c4cc2e0d66adc51ecb7facdf4e49594
Bernardo Amorim <contato@bamorim.com>
2016-07-09 18:40:46 -0300

    Sections with deepness control

--- ex4/quicksort_sections.c
+++ ex4/quicksort_sections.c
@@ -1,8 +1,23 @@
 #include "quicksort.h"
 #include "quicksort_common.c"
-void ip_quicksort(int* values, size_t start, size_t end) {
-  size_t pivot;
-  pivot = ip_partition(values, start, end);
+
+void serial_quicksort(int* values, size_t start, size_t end){
+  size_t pivot = ip_partition(values, start, end);
+
+  if(pivot > start + 1){
+    serial_quicksort(values, start, pivot-1);
+  }
+
+  if(pivot < end-1){
+    serial_quicksort(values, pivot+1, end);
+  }
+}
+
+void do_quicksort(int* values, size_t start, size_t end, int deep) {
+  size_t pivot = ip_partition(values, start, end);
+  if(deep <= 0){
+    return serial_quicksort(values, start, end);
+  }
 
   #pragma omp parallel
   {
@@ -10,13 +25,17 @@ void ip_quicksort(int* values, size_t start, size_t end) {
     {
       #pragma omp section
       if(pivot > start + 1){
-        ip_quicksort(values, start, pivot-1);
+        do_quicksort(values, start, pivot-1, deep-1);
       }
 
       #pragma omp section
       if(pivot < end-1){
-        ip_quicksort(values, pivot+1, end);
+        do_quicksort(values, pivot+1, end, deep-1);
       }
     }
   }
+}
+
+void ip_quicksort(int* values, size_t start, size_t end){
+  do_quicksort(values, start, end, 10);
 }
